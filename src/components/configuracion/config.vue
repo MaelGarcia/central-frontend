36e18520699a4cccbd63c45aed4ee834f2df3a42
mane0105 <mane.g.perez@gmail.com>
2023-08-08 22:33:49 -0600

    vista

--- src/components/configuracion/config.vue
+++ src/components/configuracion/config.vue
@@ -1,12 +1,43 @@
 <template>
-    <h2>texto 2</h2>
+    <div>
+    <div class="heading-with-button">
+      <button id="user-list-new-button" type="button" class="btn btn-primary"
+        @click="modal(true)">
+        <span>Nuevo registro</span>
+      </button>
+      Formularios con limite
+    </div>
+    <div v-if="estatusmodal">
+        <h3>aqui formulario</h3>
+        <button id="user-list-new-button" type="button" class="btn btn-primary"
+        @click="modal(false)">
+        <span>Cancelar</span>
+      </button>
+    </div>
+    <table id="user-list-table" class="table">
+      <thead>
+        <tr>
+          <th>Formulario</th>
+          <th>Limite</th>
+          <th class="actions">Opciones</th>
+        </tr>
+      </thead>
+      <tbody >
+    </tbody>
+        <td>test</td>
+        <td>test</td>
+        <td>opciones</td>
+    </table>
+    <loading :state="initiallyLoading"/>
+  </div>
 </template>
 <script>
 export default {
   name: 'Configuracion',
   data() {
     return {
-      msj: 'hola'
+      msj: 'hola',
+      estatusmodal:false
     };
   },
   mounted() {
@@ -16,7 +47,17 @@ export default {
     
   },
   methods: {
-   
+   modal(estatus) {
+    this.estatusmodal= estatus;
+   },
   }
 };
 </script>
+
+<style lang="scss">
+#user-list-table {
+  table-layout: fixed;
+
+  th.actions { width: 125px; }
+}
+</style>
